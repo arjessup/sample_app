31a95b5a00a30780b4b7d400bfc5f01eb7113757
Andrea Jessup <jessup.andrea@gmail.com>
2012-02-13 16:08:38 -0600

    Finished Chapter 3 Exercises

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ SampleApp::Application.routes.draw do
   get "pages/contact"
 
   get "pages/about"
+  
+  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
